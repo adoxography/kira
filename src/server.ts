72d69c04778fa017b851d56ebb7bb385039869ca
Graham Still <graham@gstill.dev>
2022-03-28 20:00:20 -0500

    Update default server settings

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const port = process.env.PORT || 5678;
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}!`);
 });
